052d2279d9b8fb27aeadb86a6a61f9404cd7fd04
Dana Gill <d.gill.design@gmail.com>
2015-12-20 11:54:15 -0500

    add flip over

--- js/memory.js
+++ js/memory.js
@@ -21,57 +21,45 @@ $(document).ready(function(){
 	];
 
 
-
-	// shuffle my class array
+// shuffle my classes array
+	
 	function shuffunction(cards) {
 
 		var tempCards = [];
-		var cardCt = cards.length; //16 cards
+    	var cardCount = cards.length; //16
 
-		while (cardCt > 0) {
+		while (cardCount > 0) {
 
-			// gets a random number
-			var randomCard = Math.floor(Math.random() * cards.length);
+      var randomCardNumber = Math.floor( Math.random() * ((cards.length -1) - 0) + 0);
+      //console.log(randomCardNumber);
+      
+      tempCards.push( cards[randomCardNumber] );
+	  //console.log(tempCards);
+      cards.splice( randomCardNumber, 1);
+      //console.log(cards);
 
-			// sees if that number "card" is still in the array
-			if ( randomCard in cards ) {
-
-				// if it is still in the cards array
-				// add it to the new array
-				tempCards.push( cards[randomCard] );
-
-				// delete from the old array so it doesnt get used again
-				delete cards[randomCard];
-
-				// update card count
-				cardCt--;
-			}
+      cardCount--;
+      
 		}
+    
 		return tempCards;
 	}
 
 	var cardsShuffd = shuffunction(cards);
+	//console.log(cardsShuffd);
 
 
-	// assign a class using the random array
+// assign a class using the new random array
+
 	$( ".cards" ).each(function( cardNumber ) {
+		
 		var randomClass =  cardsShuffd[ cardNumber ]
+		
 		$(this).addClass("cards " + randomClass);
-	});
-
-
-
-	// toggle the card images on click
-	$( ".cards" ).click(function() {
-		$( this ).toggleClass("flipped");
-
-		checkWinner();
-	});
-
-
 	
-	// button to reset the game
-	// fixes the empty array bug
+	});
+
+// new game button
 
 	$( ".newgame" ).hover(function() {
 		$( this ).toggleClass("refresh");
@@ -80,6 +68,88 @@ $(document).ready(function(){
 	$( ".newgame" ).click(function() {
 	    location.reload();
 	});
+
+
+
+
+	var numberofFlipped = 0;
+	var firstChoice;
+	var firstChoiceImg;
+	var secondchoice;
+	var secondchoiceimg;
+
+	//this is my counter
+	$( ".cards" ).click(function() {
+
+		numberofFlipped++
+		//console.log('number of flipped cards: '+numberofFlipped);
+		//while in cards, toggle to flipped		
+		$( this ).toggleClass("flipped");
+		// if number of "clicks" is equal to 1, var firstchoice = where
+		// you clicked first, var firstchoiceimg = the background image
+		//of what was clicked - log that image name
+		if (numberofFlipped === 1){
+			firstChoice = $(this);
+			firstChoiceImg = $(this).css('background-image');
+			//console.log(firstChoiceImg)
+		}
+
+		// if number of "clicks" is equal to 2, var secondchoice = where
+		// you clicked next, var secondchoiceimg = the background image
+		// of what was clicked - log that image name		
+
+
+		if (numberofFlipped === 2){
+			secondChoice = $(this);
+			secondChoiceImg = $(this).css('background-image');
+			//console.log(secondChoiceImg)
+
+
+			// this looks at the class of both clicked images and if they are
+			// equal, log match. (it doesn't log the match)
+			// and add the class of "found" to the matching cards
+			if ( firstChoiceImg === secondChoiceImg ){
+				console.log('match');
+				firstChoice.addClass('found');
+				secondChoice.addClass('found');
+				numberofFlipped = 0 
+			} else {
+				console.log("didnt match");
+				//if you've clicked twice....um? set numberofflipped back to 0
+				//not sure what the settimeout does
+				//for all of my cards in my card div that have been flipped and
+				numberofFlipped = 0 
+				setTimeout(function(){
+					$('.cards').each(function(card){
+						if ( this.classList.contains('found') === false ){
+							$('.cards').removeClass('flipped')
+						}
+					})
+				},1000);
+			}
+		}
+	});
+
+
+	// need a funcction to check and see if there's
+	// a match when 2 cards are flipped
+
+	//var checkWinner = function() {
+	//var flippedCards = $(".flipped");
+
+	// see if there are 2 "flipped" cards
+	// see if they have identical classes
+
+	//if yes, stay flipped
+	//i can change the class? add "found" remove "flipped"?
+
+	//else
+
+	//if not a match - unflip them
+		//remove "flipped" class	
+
+
+
 
 
 });
